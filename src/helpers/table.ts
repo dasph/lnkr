1cff435329d19b53b5eb3bcebe0bdae0aca0cd4c
Andrew Nyzhnyk <andrew@awkwrd.dev>
2023-05-21 20:03:29 +0200

    light table builder refactor

--- src/helpers/table.ts
+++ src/helpers/table.ts
@@ -1,28 +1,32 @@
 const MIN_COLUMN_WIDTH = 8
 
+const roundUpEven = (number: number): number => Math.ceil(number / 2.0) * 2
+
+const left = (value: string, length: number): string => {
+  const padding = ' '.repeat(length - value.length)
+  return ` ${value}${padding}`
+}
+
+const center = (value: string, length: number): string => {
+  const padding = ' '.repeat(roundUpEven(length - value.length) / 2)
+  return `${padding}${value}${padding}`
+}
+
 export const createTable = (data: Record<string, string | number>[]) => {
   const headerEntries = [...data.reduce<Set<string>>((ctx, curr) => {
     Object.keys(curr).map((key) => ctx.add(key))
     return ctx
   }, new Set()).values()]
 
-  const columns = headerEntries.map((value) => ({
-    value,
-    width: data.reduce((ctx, curr) => {
-      const { length } = `${curr[value]}`
-      return length > ctx ? length : ctx
-    }, MIN_COLUMN_WIDTH)
-  }))
+  const columns = headerEntries.map((value) => ({ value, width: Math.max(MIN_COLUMN_WIDTH, ...data.map((entry) => `${entry[value]}`.length)) }))
 
-  const h1 = `┌${columns.map(({ width }) => '─'.repeat(Math.ceil(width / 2.0) * 2 + 1)).join('┬')}┐`
+  return [
+    `┌${columns.map(({ width }) => '─'.repeat(roundUpEven(width) + 1)).join('┬')}┐`,
+    `│${columns.map(({ value, width }) => center(value, width)).join('│')}│`,
+    `├${columns.map(({ width }) => '─'.repeat(roundUpEven(width) + 1)).join('┼')}┤`,
 
-  const h2 = `│${columns.map(({ value, width }) => `${' '.repeat(~~((width - value.length - 0.1) / 2 + 1))}${value}${' '.repeat(~~((width - value.length - 0.1) / 2 + 1))}`).join('│')}│`
+    ...data.map((entry) => `│${columns.map(({ value, width }) => left(`${entry[value]}`, width)).join('│')}│`),
 
-  const h3 = `├${columns.map(({ width }) => '─'.repeat(Math.ceil(width / 2.0) * 2 + 1)).join('┼')}┤`
-
-  const f0 = `└${columns.map(({ width }) => '─'.repeat(Math.ceil(width / 2.0) * 2 + 1)).join('┴')}┘`
-
-  const body = data.map((entry) => `│${columns.map(({ value, width }) => ` ${entry[value]}${' '.repeat(width - `${entry[value]}`.length)}`).join('│')}│`).join('\n')
-
-  return [h1, h2, h3, body, f0].join('\n')
+    `└${columns.map(({ width }) => '─'.repeat(roundUpEven(width) + 1)).join('┴')}┘`
+  ].join('\n')
 }
