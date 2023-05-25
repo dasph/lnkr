9bd3c7e93300b59b04444f9652aa1b69a5bd1e10
Andrew Nyzhnyk <andrew@awkwrd.dev>
2023-05-25 09:15:45 +0200

    timestamp type hotfix

--- src/services/getIp.ts
+++ src/services/getIp.ts
@@ -1,5 +1,5 @@
 import type { Ip } from '~/types/mod.ts'
-import { DocumentReference, addDocument, collection, getDocs, limit, query, updateDoc, where } from '~/middleware/mod.ts'
+import { DocumentReference, Timestamp, addDocument, collection, getDocs, limit, query, updateDoc, where } from '~/middleware/mod.ts'
 
 export const getIp = async (value: number): Promise<DocumentReference<Ip>> => {
   const lastActive = new Date(new Date().setHours(0, 0, 0, 0))
@@ -8,7 +8,7 @@ export const getIp = async (value: number): Promise<DocumentReference<Ip>> => {
 
   if (empty) return addDocument({ value, lastActive, createdAt: new Date() }, 'ips')
 
-  if (lastActive !== docs[0].data().lastActive) updateDoc(docs[0].ref, { lastActive })
+  if ((docs[0].data().lastActive as unknown as Timestamp).toMillis() !== lastActive.valueOf()) updateDoc(docs[0].ref, { lastActive })
 
   return docs[0].ref
 }
