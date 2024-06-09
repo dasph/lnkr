7002d8e7e876f1c6031e69e65487b049c3f55ac1
Andrew Nyzhnyk <andrew@nyzhnyk.com>
2024-06-09 18:39:37 +0200

    ref: improved authorization middleware

--- src/middleware/authorize.ts
+++ src/middleware/authorize.ts
@@ -1,14 +1,10 @@
-import { type RouterMiddleware, Status } from 'oak'
+import { type Context, Status } from 'oak'
 
 import type { AuthorizedState } from '~/types/mod.ts'
 
 import { verify } from '~/services/mod.ts'
 
-// TODO: fix route value
-type Middleware = RouterMiddleware<'', Record<never, never>, AuthorizedState>
-type MiddlewareArgs = Parameters<Middleware>
-
-export const authorize: Middleware = async (ctx: MiddlewareArgs[0], next) => {
+export const authorize = async (ctx: Context<AuthorizedState>, next: () => Promise<unknown>) => {
   const token = await ctx.cookies.get('access-token')
   ctx.assert(token, Status.Unauthorized)
 
